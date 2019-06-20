54f3af46c47c67ee1bcbb15a531b61f748402fe5
christianbitter <christian.bitter@gmail.com>
2019-06-20 10:46:58 +0200

    added comments.

---
+++ ui.py
@@ -0,0 +1,11 @@
+# TODO: UI 2nd version
+#  - simplification in terms of hierarchy chain, there needs to be a container element abstraction
+#  - menu items are assigned to a menu, so explicit placement is not required
+#  - for improvement - ui elements should only render into their own buffer, which will be blit to the right position
+#  - font alignment left, right, center
+#  - the click arguments should be simplified into sender, args
+#  - support screen background and alpha blending
+
+
+# A screen is a visual user interface container for our game or any other pygame application.
+# It can have a menu or any other user interface component.
