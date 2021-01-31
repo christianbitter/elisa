c7ca4c10fb50f748f19f5d71c86e507491e40f8e
christian.bitter <christian.bitter@gmail.com>
2021-01-31 08:27:49 +0100

    Init

---
+++ elisa/linalg/__init__.py
@@ -0,0 +1,25 @@
+# from .geom2 import Circle2, Poly2, Rect2, Tri2
+# from .intersection import (
+#     area_of_intersection_AABB2,
+#     inside_circle2,
+#     inside_rect2,
+#     inside_square2,
+#     intersection2,
+#     intersects_AABB2,
+# )
+# from .linalg import (
+#     ALPHA_PI_INV,
+#     H2_PI,
+#     PI_INV,
+#     TWO_PI,
+#     angle_to_rad,
+#     clampf,
+#     rad_to_angle,
+# )
+# from .mat2 import Mat2, eye2, one2, zero2
+# from .mat3 import Mat3, eye3, one3, translate2D, zero3
+# from .plane2 import Plane2
+# from .ray2 import Ray2
+# from .vec import Vec, cov, mean, sd, var
+# from .vec2 import Point2, Vec2
+# from .vec3 import Point3, Vec3
